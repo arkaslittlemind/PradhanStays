6f266df6bba0790233918bacaee55eecbd074897
arkaslittlemind <arkadiptamojumder14@gmail.com>
2024-02-01 00:51:19 +0530

    added property list

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import './home.css'
 import Navbar from '../../components/navbar/Navbar'
 import Header from '../../components/header/Header'
 import Featured from '../../components/featured/Featured'
+import PropertyList from '../../components/properList/PropertyList'
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
           <div className="homeContainer">
             <Featured />
             <h1 className="homeTitle">Browse by property type</h1>
+            <PropertyList />
           </div>
     </div>
   )
