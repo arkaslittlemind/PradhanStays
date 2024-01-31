7b88405e6305a5aa7c517a72af7f0c078a7faece
arkaslittlemind <arkadiptamojumder14@gmail.com>
2024-02-01 01:00:29 +0530

    added featured properties

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import Navbar from '../../components/navbar/Navbar'
 import Header from '../../components/header/Header'
 import Featured from '../../components/featured/Featured'
 import PropertyList from '../../components/properList/PropertyList'
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties'
 
 const Home = () => {
   return (
@@ -14,6 +15,9 @@ const Home = () => {
             <Featured />
             <h1 className="homeTitle">Browse by property type</h1>
             <PropertyList />
+            <h1 className="homeTitle">Home guests love</h1>
+            <FeaturedProperties />
+            
           </div>
     </div>
   )
